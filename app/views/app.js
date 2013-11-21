2bd13c9e0d59d9398c8be823c5992948cbdd7acf
Andreas Soderstrom <as222sy@student.lnu.se>
2013-11-21 14:04:59 +0100

    Testing sync

--- app/views/app.js
+++ app/views/app.js
@@ -1,7 +1,7 @@
 define(['backbone'], function(Backbone){
 	var App = Backbone.View.extend({
 		initialize: function() {
-			console.log("Console.log skulle på svenska kunna vara Trösta.stock");
+			console.log("Console.log 222 skulle på svenska kunna vara Trösta.stock");
 		}
 	})
 	return App;
